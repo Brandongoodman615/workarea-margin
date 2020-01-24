76896c187c62b696261f2187b654ff3507d9ae5b
Brandon Goodman <brandongoodman615@gmail.com>
2020-01-23 22:46:32 -0600

    Add append points for pricing list

---
+++ config/initializers/append_points.rb
@@ -0,0 +1,11 @@
+module Workarea
+  Workarea.append_partials(
+    'admin.margin_labels',
+    'workarea/admin/pricing_skus/margin_labels'
+  )
+
+  Workarea.append_partials(
+    'admin.margin',
+    'workarea/admin/pricing_skus/margin'
+  )
+end
