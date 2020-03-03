f6f4dafb4623c0c2ecbfc78e7bb16d14eeaf51c7
Brandon Goodman <brandongoodman615@gmail.com>
2020-03-03 16:15:38 -0600

    Use existing appens

--- config/initializers/append_points.rb
+++ config/initializers/append_points.rb
@@ -8,14 +8,4 @@ module Workarea
     'admin.additional_fields',
     'workarea/admin/pricing_skus/additional_fields'
   )
-
-  Workarea.append_partials(
-    'admin.add_cost',
-    'workarea/admin/pricing_skus/add_cost'
-  )
-
-  Workarea.append_partials(
-    'admin.card_cost_and_margin',
-    'workarea/admin/pricing_skus/card_cost_and_margin'
-  )
 end
